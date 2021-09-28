b09e331a069bd5479b65d10aab429e1cfc26a69c
DWO1327 <83183741+DWO1327@users.noreply.github.com>
2021-09-28 14:26:53 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t dwo1327 .
 
-docker tag dwo1327 idi2019.azurecr.io/dwo1327
-docker push idi2019.azurecr.io/dwo1327
+#docker tag dwo1327 idi2019.azurecr.io/dwo1327
+#docker push idi2019.azurecr.io/dwo1327
